dd6e518870a7d335ebc81f46d868cc1b5a9b937e
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2023-11-17 16:08:09 +0100

    Mod setup

---
+++ src/main/java/com/ninni/onward_upward/OU.java
@@ -0,0 +1,12 @@
+package com.ninni.onward_upward;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class OU implements ModInitializer {
+	public String MOD_ID = "onward_upward";
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/ninni/onward_upward/OUClient.java
@@ -0,0 +1,12 @@
+package com.ninni.onward_upward;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+public class OUClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
